292b03117cc49d90cdd42574e5a853ad8fb54a3b
TylerCode <tyler.christenson@live.com>
2017-10-20 11:05:56 -0600

    Made some changes to the hello world equiv

--- modules/hello.py
+++ modules/hello.py
@@ -1,7 +1,14 @@
-# Written by TylerCode
-# Released under WTFPL v2
+"""Greeting/Hello module for Sopel
 
-# Super simple, nothing special
+Includes a response to the person who called it for example:
+
+.hi
+Hello, <username>
+
+Written by TylerCode
+Released under WTFPL v2
+
+"""
 
 import sopel.module
 import random
@@ -9,4 +16,4 @@ import random
 @sopel.module.commands('hello', 'hi')
 def hello(bot, trigger):
     verb = random.choice(('Hey!', 'Hello!', 'Hi!', 'Hola', 'Heyoo', 'Hey', 'Hello', 'Hi'))
-    bot.say(verb)
+    bot.say(verb + ', ' + trigger.nick)
